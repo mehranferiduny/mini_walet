80e2ea6213d7e9ae833541dd978c22022158936d
mehran feriduni <m3hr4nus@gmail.com>
2024-09-16 12:00:17 +0330

    add entity for walet and user

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { Walet } from "src/modules/walet/entities/walet.entity";
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn('increment')
+  id:number;
+  @Column()
+  full_name:string;
+  @Column()
+  phone:string;
+  @Column({type:'numeric',default:0})
+  balance:number
+  @CreateDateColumn()
+  created_at:Date
+
+  @OneToMany(()=>Walet,walet=>walet.user)
+  transaction:Walet
+
+}
--- src/modules/walet/entities/walet.entity.ts
+++ src/modules/walet/entities/walet.entity.ts
@@ -1,1 +1,20 @@
-export class Walet {}
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { WalltType } from "../walet.enum";
+import { User } from "src/modules/user/entities/user.entity";
+
+@Entity('walet')
+export class Walet {
+  @PrimaryGeneratedColumn('increment')
+  id:number
+  @Column({type:'enum',enum:WalltType})
+  type:string;
+  @Column()
+  invoice_number:string;
+  @CreateDateColumn()
+  created_at:Date
+  
+  @Column()
+  userId:number
+  @ManyToOne(()=>User,user=>user.transaction,{onDelete:'SET NULL'})
+  user:User
+}
